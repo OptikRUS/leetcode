5bbba9dad365df8f75dd9c82fcd7ceed23c30113
optikrus <optikrus61@gmail.com>
2022-05-04 14:49:49 +0300

    create table

---
+++ tasks/task_db.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name varchar(50),
+	surname varchar(50),
+	family_name varchar(50),
+	created_at DATE DEFAULT CURRENT_TIMESTAMP null,
+	updeted_at DATE DEFAULT NOW() NULL
+);
+
