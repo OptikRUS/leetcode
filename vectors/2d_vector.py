88198fb960b6cf8be59f6c52bbf42aaebb961f38
optikrus <optikrus61@gmail.com>
2022-05-13 16:52:44 +0300

    edit class 2d vector

--- vectors/2d_vector.py
+++ vectors/2d_vector.py
@@ -3,30 +3,24 @@ class Vector:
         self.x = x
         self.y = y
 
-    # длина вектора
-    def length(self):
+    def corner(self, other):
+        return (self.x * other.x + self.y * other.y) / (abs(self) * abs(other))
+
+    def scalar_mul(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def __abs__(self):
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
-    # умножение вектора на число
-    def mulnum(self, num):
+    def __mul__(self, num):
         self.x *= num
         self.y *= num
 
     def __str__(self):
         return f"coord x: {self.x}, coord y: {self.y}"
 
-    # сложение векторов
     def __add__(self, other):
         return self.x + other.x, self.y + other.y
 
-    # вычитание векторов
     def __sub__(self, other):
         return self.x - other.x, self.y - other.y
-
-    # умножение векторов
-    def __mul__(self, other):
-        return self.x * other.x + self.y * other.y
-
-    # угол между векторами
-    def __truediv__(self, other):
-        return (self.x * other.x + self.y * other.y) / (self.length() * other.length())
