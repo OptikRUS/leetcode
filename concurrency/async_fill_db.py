ffa30933c182143947baa48e328364db02b7f8de
optikrus <optikrus61@gmail.com>
2022-05-21 00:30:29 +0300

    sync vs async

--- concurrency/async_fill_db.py
+++ concurrency/async_fill_db.py
@@ -14,26 +14,14 @@ QUERY = """
 
 async def fill_table_users(db_pool):
     await db_pool.fetch(QUERY, 'username', 'lastname', 18)
-    # async with connect.transaction():
-    #     await connect.execute(QUERY)
 
 
 async def main():
 
     db_pool = await asyncpg.create_pool(f"postgresql://{username}:{password}@localhost:5432/pdb")
 
-    # connect = await asyncpg.connect(
-    #     database="pdb",
-    #     user=username,
-    #     password=password,
-    #     host="127.0.0.1",
-    #     port=5432
-    # )
-    # connect = await asyncpg.connect("postgresql://optikrus:password123456@localhost:5432/pdb")
-
     for i in range(10000):
         asyncio.ensure_future(coro_or_future=fill_table_users(db_pool))
-        # ensure_future
 
 
 if __name__ == "__main__":
