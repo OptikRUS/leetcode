881133c9527b479380c12b3619c72f85b47074b8
optikrus <optikrus61@gmail.com>
2022-05-25 11:18:16 +0300

    capture

--- concurrency/uid.py
+++ concurrency/uid.py
@@ -27,14 +27,20 @@ async def get_uid():
 
 
 async def main():
+    for i in range(5):
+        asyncio.ensure_future(get_uid())
+
     aws = []
     for i in range(5):
         aws.append(get_uid())
     await asyncio.gather(*aws)
 
-    future_aws = []
-    for i in range(5):
-        future_aws.append(asyncio.ensure_future(get_uid()))
-    await asyncio.gather(*future_aws)
 
 asyncio.run(main())
+
+
+"""
+Если цикл с ensure_future выполнить после цикла с gather, то от первого мы результата не увидим.
+Если перед ensure_future поставить await6 то будем ждать выполнения каждой функции по 2 секунды.
+Если использовать стандартный sleep, то функции будут работать синхронно.
+"""
